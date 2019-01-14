7f4928825c18a6e5b274747f4241f3813a4d0df6
Meeko <lya121@gmail.com>
2019-01-14 11:19:00 +0800

    跟新双色logo

--- src/layouts/Sider/index.js
+++ src/layouts/Sider/index.js
@@ -3,7 +3,7 @@ import { Layout } from 'antd';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
 import Menu from './Menu';
-import logo from '@src/assets/logo-white.svg';
+import logo from '@src/assets/logo_white.png';
 import styles from '../styles.less';
 
 export default class GSider extends PureComponent {
@@ -13,7 +13,7 @@ export default class GSider extends PureComponent {
             <Layout.Sider className={styles.sider} trigger={null} collapsible collapsed={collapsed} width="240">
                 <div className={classnames(styles.logo, { [`${styles.collapsed}`]: collapsed })}>
                     <Link to="/">
-                        {/* <img alt="Workaholic" src={logo} /> */}
+                        <img alt="Workaholic" src={logo} />
                         <h1>白叶子</h1>
                     </Link>
                 </div>
