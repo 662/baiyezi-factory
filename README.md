899e392fb82bf613593e2a5df845a0507d93188e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-15 08:36:04 +0000

    Deploy 662/baiyezi-factory to github.com/662/baiyezi-factory.git:master

---
+++ README.md
@@ -0,0 +1,8 @@
+# baiyezi-factory
+基于umi,dva,antd的项目骨架，源代码在 _source_ 分支，master用于github pages预览的，是 _source_ 分支构建之后的产物
+
+### 注意
+
+* 使用时需根据需要修改或删除 _./.travis.yml_
+* 使用时需根据需要修改或删除 _./.umirc.js_ 中的 ```base: 'baiyezi-factory',```
+* 使用时需根据需要修改或删除 _./.umirc.js_ 中的 ```publicPath: '/baiyezi-factory/static/',```
---
+++ precache-manifest.ee30c35bc2b9582926796ae0200e804d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fe85f6d74fbebd1557c",
+    "url": "/baiyezi-factory/umi.css"
+  },
+  {
+    "revision": "3fe85f6d74fbebd1557c",
+    "url": "/baiyezi-factory/umi.js"
+  },
+  {
+    "revision": "a97b4252b3b8923656bf5a1c53bd17c9",
+    "url": "/baiyezi-factory/static/sidebar-1.a97b4252.jpg"
+  },
+  {
+    "revision": "3b5ada8321cd8acd960688d5609b106b",
+    "url": "/baiyezi-factory/index.html"
+  },
+  {
+    "revision": "8b684a9d7e1ef370bbb5e5c3adb0e7a3",
+    "url": "/baiyezi-factory/404.html"
+  },
+  {
+    "revision": "60f00615a9bcd47f3be571dbe80775fa",
+    "url": "/baiyezi-factory/README.md"
+  },
+  {
+    "revision": "99791b7d051d00015037c357d8663c7d",
+    "url": "/baiyezi-factory/manifest.json"
+  },
+  {
+    "revision": "b19327e3e83ce06113ae14c0d0497903",
+    "url": "/baiyezi-factory/pwacompat.min.js"
+  }
+];
